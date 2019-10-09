9aafa0882b212017575e79a564b90aa7a58d8319
Glitch (respected-apricot) <none>
2019-10-09 22:58:42 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Purpose:
+  This is a subnet practice application with step by step solutions.
+  Other resources I've visited while learning this material only provided answers, without a step by step explanation. 
+  
+
---
+++ public/decimalToBinary.js
@@ -0,0 +1,19 @@
+function decimalToBinary(input) {
+  let leadingZero = '';
+  let binaryLength;
+  let results;
+  let binary = input.toString(2);
+  binaryLength = binary.length;
+  
+  if (binaryLength < 8) {
+    while (binaryLength < 8) {
+      leadingZero += '0'
+      binaryLength += 1;
+    }
+    results = leadingZero.concat(binary);
+    return results
+  } else {
+    return binary    
+  }
+  
+}
